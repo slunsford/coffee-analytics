9aed360146484681774428653e9e64f7c5198bb3
Sean Lunsford <sean@lunsford.tech>
2023-08-22 23:01:20 +0300

    Fix staging models

--- models/staging/coffee/stg_coffee__origins.sql
+++ models/staging/coffee/stg_coffee__origins.sql
@@ -1,11 +1,12 @@
-with source as ( select * from {{ source('coffee', 'ratings') }} ),
+with source as ( select * from {{ source('coffee', 'origins') }} ),
 
      renamed as (
-         select _airtable_id as rating_id,
-                regexp_extract(coffee, 'rec\w+') as coffee_id,
-                date as rating_date,
-                rating,
-                -- date_override as rating_date_override,
+         select _airtable_id as origin_id,
+                name as country_name,
+                -- flag,
+                region as world_region,
+                -- coffees,
+                -- rating,
                 _airtable_created_time
            from source
      )
--- models/staging/coffee/stg_coffee__ratings.sql
+++ models/staging/coffee/stg_coffee__ratings.sql
@@ -1,12 +1,11 @@
-with source as ( select * from {{ source('coffee', 'origins') }} ),
+with source as ( select * from {{ source('coffee', 'ratings') }} ),
 
      renamed as (
-         select _airtable_id as origin_id,
-                name as country_name,
-                -- flag,
-                region as world_region,
-                -- coffees,
-                -- rating,
+         select _airtable_id as rating_id,
+                regexp_extract(coffee, 'rec\w+') as coffee_id,
+                date as rating_date,
+                rating,
+                -- date_override as rating_date_override,
                 _airtable_created_time
            from source
      )
