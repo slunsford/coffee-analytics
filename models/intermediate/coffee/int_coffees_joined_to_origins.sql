3de843a93a2bf3a6ebab0f1dff85aaefb07a8901
Sean Lunsford <sean@lunsford.tech>
2023-08-22 23:04:32 +0300

    Create intermediate models

---
+++ models/intermediate/coffee/int_coffees_joined_to_origins.sql
@@ -0,0 +1,20 @@
+with
+
+coffees as (
+	select * from {{ ref('int_coffees_with_flavor_profile_key') }}
+),
+
+origins as (
+	select * from {{ ref('stg_coffee__origins') }}
+),
+
+coffees_and_origins_joined as (
+	select coffees.*,
+		   coalesce(origins.country_name, 'Blend') as country,
+		   coalesce(origins.world_region, 'Blend') as world_region
+	  from coffees
+ left join origins
+		on coffees.origin_id = origins.origin_id
+)
+
+select * from coffees_and_origins_joined
