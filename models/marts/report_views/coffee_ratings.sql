c586248ecea0077b1f86326f91eeea2efa7c9aa3
Sean Lunsford <sean@lunsford.tech>
2023-10-03 11:50:22 +0300

    Create report views

---
+++ models/marts/report_views/coffee_ratings.sql
@@ -0,0 +1,11 @@
+ select coffee_name     as "Coffee",
+        roaster         as "Roaster",
+        country         as "Country",
+        world_region    as "World Region",
+        rating          as "Rating",
+        rated_date      as "Rated Date",
+        weight          as "Weight",
+        weighted_rating as "Weighted Rating"
+   from {{ ref('coffees') }}
+   join {{ ref('ratings') }}
+  using (coffee_id)
---
+++ models/marts/report_views/flavor_ratings.sql
@@ -0,0 +1,17 @@
+ select flavor          as "Flavor",
+        flavor_category as "Flavor Category",
+        coffee_name     as "Coffee",
+        roaster         as "Roaster",
+        country         as "Country",
+        world_region    as "World Region",
+        rating          as "Rating",
+        rated_date      as "Rated Date",
+        weight          as "Weight",
+        weighted_rating as "Weighted Rating"
+   from {{ ref('flavors') }}
+   join {{ ref('flavor_profiles') }}
+  using (flavor_id)
+   join {{ ref('coffees') }}
+  using (flavor_profile_key)
+   join {{ ref('ratings') }}
+  using (coffee_id)
