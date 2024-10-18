80dc50db53ddc699ab36b3ee74728a33eac5ec93
Sean Lunsford <sean@seanlunsford.com>
2024-10-19 00:58:31 +0300

    Add flavor_categories

--- models/marts/flavors.sql
+++ models/marts/flavors.sql
@@ -4,11 +4,11 @@ flavors as (
     select * from {{ ref('stg_airtable__flavors') }}
 ),
 
-flavors_with_ratings as (
+select_columns as (
   
      select flavor_id,
             flavor,
-            -- flavor_category_group_key,
+            flavor_category_group_key,
             flavor_categories[1] as primary_flavor_category,
             list_aggregate(flavor_categories, 'string_agg', ', ') as flavor_categories
             
@@ -16,4 +16,4 @@ flavors_with_ratings as (
        
 )
 
-select * from flavors_with_ratings
+select * from select_columns
